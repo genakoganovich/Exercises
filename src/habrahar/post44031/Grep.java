f362d24afb643891677fa133b4c6b905f68470bc
Gennadiy Koganovich <genakoganovich@gmail.com>
2017-06-24 14:47:10 +0300

    00004: output result to file

--- src/habrahar/post44031/Grep.java
+++ src/habrahar/post44031/Grep.java
@@ -6,21 +6,25 @@ import java.util.ArrayList;
 class Grep {
     private String inputName;
     private String outputName;
+    private String outputResultName;
     private String sample;
     private ArrayList<String> lines;
 
     Grep() {
         inputName = "c:\\Users\\gennady\\IdeaProjects\\Exercises\\src\\habrahar\\post44031\\input.txt";
         outputName = "c:\\Users\\gennady\\IdeaProjects\\Exercises\\src\\habrahar\\post44031\\output.txt";
+        outputResultName = "c:\\Users\\gennady\\IdeaProjects\\Exercises\\src\\habrahar\\post44031\\outputResult.txt";
         sample = "a";
         lines = new ArrayList<>();
     }
-    private void doGrep() {
+    private void doGrep() throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(outputResultName)));
         for (String str: lines) {
             if(str.contains(sample)) {
-                System.out.println(str);
+                bufferedWriter.write(str + " contains " + sample + "\r\n");
             }
         }
+        bufferedWriter.close();
     }
     private void readInput() throws IOException {
         File file = new File(inputName);
