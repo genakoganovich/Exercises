fcea0a06c624f53919d7558454fa2ff7fc9d7d5f
Gennadiy Koganovich <genakoganovich@gmail.com>
2017-06-24 18:29:49 +0300

    00007: server

--- src/habrahar/post44031/chat/Server.java
+++ src/habrahar/post44031/chat/Server.java
@@ -1,4 +1,28 @@
 package habrahar.post44031.chat;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = 1234;
+        ServerSocket ss = new ServerSocket(port);
+        System.out.println("server is waiting...");
+        Socket socket = ss.accept();
+        System.out.println("client is accepted");
+        InputStream sin = socket.getInputStream();
+        OutputStream sout = socket.getOutputStream();
+
+        DataInputStream in = new DataInputStream(sin);
+        DataOutputStream out = new DataOutputStream(sout);
+
+        String line;
+        while (true) {
+            line = in.readUTF();
+            System.out.println(line);
+            //out.writeUTF(line);
+            //out.flush();
+        }
+    }
 }
